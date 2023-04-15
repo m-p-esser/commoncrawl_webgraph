c52922e20d6a2d4e56caf0f3944be2898168eabe
Marc-Philipp Esser <m-esser@mail.de>
2023-04-16 00:10:44 +0200

    Add hello, world flow

---
+++ src/flows/hello_world.py
@@ -0,0 +1,11 @@
+from prefect import flow, get_run_logger
+from platform import node, platform
+
+@flow(name="hello_world_flow")
+def hello_world_flow():
+    logger = get_run_logger()
+    logger.info("Hello, World!")
+    logger.info(f"Running on Network '{node()}' and Instance '{platform()}'")
+
+if __name__ == "__main__":
+    hello_world_flow()
